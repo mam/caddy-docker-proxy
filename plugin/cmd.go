e28008f507ff422aef4ef9b46358e4d171ba1caa
Martin Mortensen <martin@pegi.io>
2021-04-06 19:16:37 +0200

    Fix listen

--- plugin/cmd.go
+++ plugin/cmd.go
@@ -108,7 +108,7 @@ func getAdminListen(options *config.Options) string {
 			}
 		}
 	}
-	return "tcp/localhost:2019"
+	return "localhost:2019"
 }
 
 func createOptions(flags caddycmd.Flags) *config.Options {
